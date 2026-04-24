4894d517d08a06bb8418eeb805ffdc4f1db4d025
AyaanZaveri <ayaanzaveri08@gmail.com>
2026-04-24 14:50:47 -0400

    updating babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const workletsPluginOptions = {
+    bundleMode: true,
+    workletizableModules: ['remend'],
+  };
+
   return {
     presets: [['babel-preset-expo']],
-    plugins: ['react-native-worklets/plugin'],
+    plugins: [['react-native-worklets/plugin', workletsPluginOptions]],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,17 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+const { getBundleModeMetroConfig } = require('react-native-worklets/bundleMode');
 const { withUniwindConfig } = require('uniwind/metro');
 
-const config = getDefaultConfig(__dirname);
+let config = getDefaultConfig(__dirname);
+
+// Worklets bundle mode generates files in a hidden `.worklets` directory.
+// Metro can miss new files there when using Watchman, so watch the package
+// root directly and fall back to the Node watcher.
+config.watchFolders.push(path.resolve(__dirname, 'node_modules/react-native-worklets'));
+config.useWatchman = false;
+
+config = getBundleModeMetroConfig(config);
 
 module.exports = withUniwindConfig(config, {
   cssEntryFile: './global.css',
